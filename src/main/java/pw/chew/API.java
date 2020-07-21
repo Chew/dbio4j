47c3503238f50a9148ae47905f5d9d7a3008d9ac
Chew <chew@chew.pw>
2020-07-20 19:58:14 -0500

    Initial API and base files

---
+++ src/main/java/pw/chew/API.java
@@ -0,0 +1,38 @@
+package pw.chew;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class API {
+    private static OkHttpClient client;
+    private final String baseUrl = "https://api.discord.bio/";
+
+    public API() {
+        client = new OkHttpClient();
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public String get(String path) {
+        Request request = new Request.Builder()
+                .url(baseUrl + path)
+                .get()
+                .build();
+
+        return performRequest(request);
+    }
+
+    public String performRequest(Request request) {
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/pw/chew/DBioAPI.java
@@ -0,0 +1,11 @@
+package pw.chew;
+
+public class DBioAPI {
+    private final API api;
+
+    public DBioAPI() {
+        api = new API();
+    }
+
+    
+}
