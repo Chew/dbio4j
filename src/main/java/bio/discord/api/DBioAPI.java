23a4bea5ead2e748fa85695c3b74f54681cb87ad
Chew <chew@chew.pw>
2020-09-01 00:08:15 -0500

    Return null if user doesn't exist

--- src/main/java/bio/discord/api/DBioAPI.java
+++ src/main/java/bio/discord/api/DBioAPI.java
@@ -1,6 +1,7 @@
 package bio.discord.api;
 
 import bio.discord.api.objects.User;
+import org.jetbrains.annotations.Nullable;
 import org.json.JSONObject;
 
 public class DBioAPI {
@@ -13,10 +14,14 @@ public class DBioAPI {
     /**
      * Load a user
      * @param id the user ID or name of the requested user.
-     * @return the new user object
+     * @return the new user object, or null if the user doesn't exist
      */
+    @Nullable
     public User getUser(String id) {
         JSONObject response = new JSONObject(api.get("user/details/" + id));
+
+        if(response.has("message"))
+            return null;
 
         return new User(response);
     }
